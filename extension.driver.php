1b66d3c0577639063dcdea5d468c6e7773b79814
Nils Werner <nils.werner@gmail.com>
2010-07-26 12:53:37 +0200

    Renamed class, added viewport and stylesheet for devs

--- extension.driver.php
+++ extension.driver.php
@@ -1,6 +1,6 @@
 <?php
 
-	Class extension_enable_tabkey extends Extension{
+	Class extension_mobile_phone_backend extends Extension{
 	
 		public function about(){
 			return array('name' => 'Mobile Phone Backend',
@@ -26,7 +26,12 @@
 		public function initaliseAdminPageHead($context) {
 			$page = $context['parent']->Page;
 			
-			$page->addStylesheetToHead(URL . '/extensions/mobile_phone_Backend/assets/stylesheet.css', 'only screen and (max-device-width: 480px)', 50002);
+			//$page->addStylesheetToHead(URL . '/extensions/mobile_phone_Backend/assets/stylesheet.css', 'only screen and (max-device-width: 480px)', 50002);
+			$page->addStylesheetToHead(URL . '/extensions/mobile_phone_Backend/assets/stylesheet.css', 'screen', 50002);
+			if($this->_Parent->Author->isDeveloper()) { /* meh, I need to do this to hide the Blueprints/System menu items */
+				$page->addStylesheetToHead(URL . '/extensions/mobile_phone_Backend/assets/dev-stylesheet.css', 'screen', 50003);
+			}
+			$page->addElementToHead(new XMLElement("meta", $style, array("name" => "viewport", "content" => "width=240,user-scalable=no")));
 		}
 			
 	}
