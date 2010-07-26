552a75cb5a4282cb4e2357e14908c991b14d4a1a
Nils Werner <nils.werner@gmail.com>
2010-07-26 12:58:49 +0200

    Rolled out CSS media query

--- extension.driver.php
+++ extension.driver.php
@@ -26,11 +26,16 @@
 		public function initaliseAdminPageHead($context) {
 			$page = $context['parent']->Page;
 			
-			//$page->addStylesheetToHead(URL . '/extensions/mobile_phone_Backend/assets/stylesheet.css', 'only screen and (max-device-width: 480px)', 50002);
-			$page->addStylesheetToHead(URL . '/extensions/mobile_phone_Backend/assets/stylesheet.css', 'screen', 50002);
+			$css_query = 'only screen and (max-device-width: 480px)';  // CSS Query for mobile devices
+			//$css_query = 'screen'; // CSS Query for debugging
+			
+			// Stylesheets
+			$page->addStylesheetToHead(URL . '/extensions/mobile_phone_Backend/assets/stylesheet.css', $css_query, 50002);
 			if($this->_Parent->Author->isDeveloper()) { /* meh, I need to do this to hide the Blueprints/System menu items */
-				$page->addStylesheetToHead(URL . '/extensions/mobile_phone_Backend/assets/dev-stylesheet.css', 'screen', 50003);
+				$page->addStylesheetToHead(URL . '/extensions/mobile_phone_Backend/assets/dev-stylesheet.css', $css_query, 50003);
 			}
+			
+			// Viewport
 			$page->addElementToHead(new XMLElement("meta", $style, array("name" => "viewport", "content" => "width=240,user-scalable=no")));
 		}
 			
